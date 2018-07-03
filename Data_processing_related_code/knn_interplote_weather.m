21ae7d31d80dc3c9f0968fe7a009e7c9234bba0a
wuyujack (Jason Leung) <mingfuliang2020@u.northwestern.edu>
2018-07-03 12:13:53 +0800

    Update knn_interplote_weather.m

--- Data_processing_related_code/knn_interplote_weather.m
+++ Data_processing_related_code/knn_interplote_weather.m
@@ -1,56 +1,58 @@
+%%% author: Jason Leung
+
 function W=knn_interplote_weather(temp_meo,d)
 %W=1;
     
-    %[d,~,~]=xlsread('station_AQI');%�����������վ�ľ�γ������
+    %[d,~,~]=xlsread('station_AQI');%导入空气质量站的经纬度坐标
     temp=zeros(size(d,1),size(temp_meo,1));
 
-    %%%%%���18������۲�վ��35����������վ����֮���ŷʽ����%%%%
+    %%%%%求出18个气象观测站到35个空气质量站两两之间的欧式距离%%%%
     for i=1:size(d,1)
         for  j=1:size(temp_meo,1) 
-            d(i,j+6)=sqrt((d(i,1)-temp_meo(j,1))^2+(d(i,2)-temp_meo(j,2))^2);%ŷʽ����=���ţ���x1-x2��^2+(y1-y2)
+            d(i,j+6)=sqrt((d(i,1)-temp_meo(j,1))^2+(d(i,2)-temp_meo(j,2))^2);%欧式距离=根号（（x1-x2）^2+(y1-y2)
         end
-        temp(i,1:size(temp_meo,1))=sort(d(i,7:size(temp_meo,1)+6));%%�Ծ����������
+        temp(i,1:size(temp_meo,1))=sort(d(i,7:size(temp_meo,1)+6));%%对距离进行排序
     end
 
-    %%%%���18������۲�վ��u��v����������ٵķ���
-    u=zeros(size(temp_meo,1),1); %��ʼ��u�����ϵķ���
-    v=zeros(size(temp_meo,1),1); %��ʼ��v�����ϵķ���
-    for j = 1:size(temp_meo,1) %���������ĳ��ʱ�̵�����վ�ķ���
-        if isnan(temp_meo(j,7)) %������ж�Ӧ������վ�ķ���ΪNaN
-            u(j)=666666666; %�Բ����ڷ��ټ�¼������վ��u������ֵΪ666666666
-            v(j)=666666666; %�Բ����ڷ��ټ�¼������վ��u������ֵΪ666666666
+    %%%%求出18个气象观测站在u、v方向上面风速的分量
+    u=zeros(size(temp_meo,1),1); %初始化u方向上的风速
+    v=zeros(size(temp_meo,1),1); %初始化v方向上的风速
+    for j = 1:size(temp_meo,1) %对于输入的某个时刻的气象站的风速
+        if isnan(temp_meo(j,7)) %如果该行对应的气象站的风速为NaN
+            u(j)=666666666; %对不存在风速记录的气象站的u分量赋值为666666666
+            v(j)=666666666; %对不存在风速记录的气象站的u分量赋值为666666666
         elseif temp_meo(j,7)>=999016
-            u(j)=0.5.*rand();  %���ڷ������999016������۲�վ���ԣ����ǵķ���С��0.5������������һ��С��0.5����0����
-            v(j)=0.5.*rand();  %���ڷ������999016������۲�վ���ԣ����ǵķ���С��0.5������������һ��С��0.5����0����
+            u(j)=0.5.*rand();  %对于风向大于999016的气象观测站而言，他们的风速小于0.5，因此随机生成一个小于0.5大于0的数
+            v(j)=0.5.*rand();  %对于风向大于999016的气象观测站而言，他们的风速小于0.5，因此随机生成一个小于0.5大于0的数
         else
-            u(j) = temp_meo(j,8) * sind((temp_meo(j,7)));%u�������
-            v(j) = temp_meo(j,8) * cosd((temp_meo(j,7)));%v�������
+            u(j) = temp_meo(j,8) * sind((temp_meo(j,7)));%u方向分量
+            v(j) = temp_meo(j,8) * cosd((temp_meo(j,7)));%v方向分量
         end
     end
 
-    k=4; %���������ڵ���2
-    h=2; %������ľ��������
-    W=[size(d,1),3,1];%��35����������վ��Ŀ����һ����ά����
-    ufenzi=0; %��ʼ�������Ȩ���u������ٵķ���
-    ufenmu=0;%��ʼ�������Ȩ���u������ٵķ�ĸ
-    vfenzi=0;%��ʼ�������Ȩ���v������ٵķ���
-    vfenmu=0;%��ʼ�������Ȩ���v������ٵķ�ĸ
-    temp_fenzi=0;%��ʼ�������Ȩ����¶ȵķ���
-    temp_fenmu=0;%��ʼ�������Ȩ����¶ȵķ�ĸ
-    pressure_fenzi=0;%��ʼ�������Ȩ���ѹǿ�ķ���
-    pressure_fenmu=0;%��ʼ�������Ȩ���ѹǿ�ķ�ĸ
-    humidity_fenzi=0;%��ʼ�������Ȩ���ʪ�ȵķ���
-    humidity_fenmu=0;%��ʼ�������Ȩ���ʪ�ȵķ�ĸ
+    k=4; %近邻数大于等于2
+    h=2; %反距离的距离的幂数
+    W=[size(d,1),3,1];%以35个空气质量站数目构造一个三维数组
+    ufenzi=0; %初始化反向加权求解u方向风速的分子
+    ufenmu=0;%初始化反向加权求解u方向风速的分母
+    vfenzi=0;%初始化反向加权求解v方向风速的分子
+    vfenmu=0;%初始化反向加权求解v方向风速的分母
+    temp_fenzi=0;%初始化反向加权求解温度的分子
+    temp_fenmu=0;%初始化反向加权求解温度的分母
+    pressure_fenzi=0;%初始化反向加权求解压强的分子
+    pressure_fenmu=0;%初始化反向加权求解压强的分母
+    humidity_fenzi=0;%初始化反向加权求解湿度的分子
+    humidity_fenmu=0;%初始化反向加权求解湿度的分母
 
-    for i=1:size(d,1)%��35����������վ�ֱ������ͷ���
-        [~,n]=find(d(i,:)==temp(i,1));%ֻ������������ڷ��٣�����ǡ�1��
+    for i=1:size(d,1)%对35个空气质量站分别求风向和风速
+        [~,n]=find(d(i,:)==temp(i,1));%只保留保留最近邻风速，因此是“1”
         W(i,9,1)=temp_meo(n-6,3);
-        for l=2:k %k�ǹ涨��ѡ��������ڵ�����۲�վ�ĸ���
+        for l=2:k %k是规定的选择的最大近邻的气象观测站的个数
             
             
-            if temp(i,l)-temp(i,1)<0.1 %�����l������۲�վ����i����������վ�ľ�����˿�������վ�����������۲�վС��0.1
+            if temp(i,l)-temp(i,1)<0.1 %如果第l个气象观测站到第i个空气质量站的距离与此空气质量站的最近邻气象观测站小于0.1
             
-            [~,n]=find(d(i,:)==temp(i,l-1));%��������ڷ��ٲ��Ҵ�l��ʼѭ��Ѱ�Ҿ����������վ��Զ������۲�վ
+            [~,n]=find(d(i,:)==temp(i,l-1));%保留最近邻风速并且从l开始循环寻找距离空气质量站更远的气象观测站
             ufenzi= ufenzi + u(n-6)/(d(i,n)^h); 
             ufenmu= ufenmu + 1/(d(i,n)^h);
             vfenzi= vfenzi + v(n-6)/(d(i,n)^h); 
@@ -62,11 +64,11 @@ function W=knn_interplote_weather(temp_meo,d)
             humidity_fenzi=humidity_fenzi+temp_meo(n-6,6)/(d(i,n)^h);
             humidity_fenmu=humidity_fenmu+1/(d(i,n)^h);
             
-            elseif temp(i,2)-temp(i,1)>0.1 || temp(i,2)>0.2 %�������ݿ��ӻ��۲⣬���ڶ����ڵ�����۲�վ������ڹ۲�վ�Ĳ�ֵ����0.1
-                                                                                   %���ߵڶ����ڹ۲�վ�������0.2��ʱ�򣬴�ʱ������Ϊֻ�������������۲�
-                                                                                   %վ�ķ��ٲ�ֱ�Ӹ��Ƹ������������վ����Ϊ�ӿ��ӻ���ͼ������ЩԶ��󲿷�
-                                                                                   %���������۲�վ����Щ���������۲�վ������ֻ��1������2������۲�վ
-            [~,n]=find(d(i,:)==temp(i,1));%ֻ������������ڷ��٣�����ǡ�1��
+            elseif temp(i,2)-temp(i,1)>0.1 || temp(i,2)>0.2 %经过数据可视化观测，当第二近邻的气象观测站与最近邻观测站的差值大于0.1
+                                                                                   %或者第二近邻观测站距离大于0.2的时候，此时我们认为只保留最近邻气象观测
+                                                                                   %站的风速并直接复制给这个空气质量站。因为从可视化的图上面那些远离大部分
+                                                                                   %空气质量观测站的那些空气质量观测站附近都只有1个或者2个气象观测站
+            [~,n]=find(d(i,:)==temp(i,1));%只保留保留最近邻风速，因此是“1”
             ufenzi= u(n-6)/(d(i,n)^h); 
             ufenmu= 1/(d(i,n)^h);
             vfenzi= v(n-6)/(d(i,n)^h); 
@@ -82,12 +84,12 @@ function W=knn_interplote_weather(temp_meo,d)
         end
             W(i,1,1)=d(i,1);
             W(i,2,1)=d(i,2);
-            W(i,3,1)=temp_fenzi/temp_fenmu; %���ݷ����Ȩ����¶�
-            W(i,4,1)=pressure_fenzi/pressure_fenmu; %���ݷ����Ȩ���ѹǿ
-            W(i,5,1)=humidity_fenzi/humidity_fenmu; %���ݷ����Ȩ���ʪ��
-            W(i,6,1)=ufenzi/ufenmu; %���ݷ����Ȩ���u����ķ���
-            W(i,7,1)=vfenzi/vfenmu; %���ݷ����Ȩ���v����ķ���
-            W(i,8,1)=sqrt(W(i,6)^2+W(i,7)^2); %���������η���(���ɶ���)�ϳɷ���
+            W(i,3,1)=temp_fenzi/temp_fenmu; %根据反向加权求出温度
+            W(i,4,1)=pressure_fenzi/pressure_fenmu; %根据反向加权求出压强
+            W(i,5,1)=humidity_fenzi/humidity_fenmu; %根据反向加权求出湿度
+            W(i,6,1)=ufenzi/ufenmu; %根据反向加权求出u方向的风速
+            W(i,7,1)=vfenzi/vfenmu; %根据反向加权求出v方向的风速
+            W(i,8,1)=sqrt(W(i,6)^2+W(i,7)^2); %利用三角形法则(勾股定理)合成风速
     end
     
 end
