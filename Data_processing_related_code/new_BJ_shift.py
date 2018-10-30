36a15fab1cbd15d1cbe31eff79f0c3ecf2c0c57d
wuyujack (Jason Leung) <mingfuliang2020@u.northwestern.edu>
2018-10-30 16:34:29 -0500

    Update new_BJ_shift.py
    Add English comment about feature engineering

--- Data_processing_related_code/new_BJ_shift.py
+++ Data_processing_related_code/new_BJ_shift.py
@@ -18,7 +18,8 @@ kdd_data = pd.read_csv(r"F:\temp_BJ_17_01_18_05-20_data_weather_75_model_full_re
 #del kdd_data['hour']
 print('Data import time[{}]'.format(time.time() - st_time))
 
-#t1风向编码
+# Encoding for t1 (Feature name) winddirection
+# t1风向编码
 kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']<=45) & (kdd_data['t1_winddirection']>=0)]=1
 kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']>45) & (kdd_data['t1_winddirection']<=90)]=2
 kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']>90) & (kdd_data['t1_winddirection']<=135)]=3
@@ -28,7 +29,8 @@ kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']>225) & (kdd_data['t1_
 kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']>270) & (kdd_data['t1_winddirection']<=315)]=7
 kdd_data['t1_winddirection'][(kdd_data['t1_winddirection']>315) & (kdd_data['t1_winddirection']<=360)]=8
 
-#knn1风向编码
+# Encoding for knn1 (Feature name) winddirection
+# knn1风向编码
 kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']<=45) & (kdd_data['knn1_winddirection']>=0)]=1
 kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']>45) & (kdd_data['knn1_winddirection']<=90)]=2
 kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']>90) & (kdd_data['knn1_winddirection']<=135)]=3
@@ -38,7 +40,8 @@ kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']>225) & (kdd_data[
 kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']>270) & (kdd_data['knn1_winddirection']<=315)]=7
 kdd_data['knn1_winddirection'][(kdd_data['knn1_winddirection']>315) & (kdd_data['knn1_winddirection']<=360)]=8
 
-#knn2风向编码
+# Encoding for knn2 (Feature name) winddirection
+# knn2风向编码
 kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']<=45) & (kdd_data['knn2_winddirection']>=0)]=1
 kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']>45) & (kdd_data['knn2_winddirection']<=90)]=2
 kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']>90) & (kdd_data['knn2_winddirection']<=135)]=3
@@ -48,6 +51,7 @@ kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']>225) & (kdd_data[
 kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']>270) & (kdd_data['knn2_winddirection']<=315)]=7
 kdd_data['knn2_winddirection'][(kdd_data['knn2_winddirection']>315) & (kdd_data['knn2_winddirection']<=360)]=8
 
+# Encoding for knn3 (Feature name) winddirection
 #knn3风向编码
 kdd_data['knn3_winddirection'][(kdd_data['knn3_winddirection']<=45) & (kdd_data['knn3_winddirection']>=0)]=1
 kdd_data['knn3_winddirection'][(kdd_data['knn3_winddirection']>45) & (kdd_data['knn3_winddirection']<=90)]=2
@@ -58,7 +62,8 @@ kdd_data['knn3_winddirection'][(kdd_data['knn3_winddirection']>225) & (kdd_data[
 kdd_data['knn3_winddirection'][(kdd_data['knn3_winddirection']>270) & (kdd_data['knn3_winddirection']<=315)]=7
 kdd_data['knn3_winddirection'][(kdd_data['knn3_winddirection']>315) & (kdd_data['knn3_winddirection']<=360)]=8
 
-#knn3风向编码
+# Encoding for knn4 (Feature name) winddirection
+# knn4风向编码
 kdd_data['knn4_winddirection'][(kdd_data['knn4_winddirection']<=45) & (kdd_data['knn4_winddirection']>=0)]=1
 kdd_data['knn4_winddirection'][(kdd_data['knn4_winddirection']>45) & (kdd_data['knn4_winddirection']<=90)]=2
 kdd_data['knn4_winddirection'][(kdd_data['knn4_winddirection']>90) & (kdd_data['knn4_winddirection']<=135)]=3
@@ -69,14 +74,18 @@ kdd_data['knn4_winddirection'][(kdd_data['knn4_winddirection']>270) & (kdd_data[
 kdd_data['knn4_winddirection'][(kdd_data['knn4_winddirection']>315) & (kdd_data['knn4_winddirection']<=360)]=8
 
 #kdd_data.to_csv(r"F:/temp_BJ_winddirection_encoding.csv",index = False,encoding = 'utf-8')
-#按站点及时间排序，必须排序
-#kdd_data = pd.DataFrame(kdd_data).sort_values(['station_id','utc_time']).reset_index(drop=True)
+
+# Sorting based on staion and time
+# 按站点及时间排序
+# kdd_data = pd.DataFrame(kdd_data).sort_values(['station_id','utc_time']).reset_index(drop=True)
+
 del kdd_data['knn1_station'] 
 del kdd_data['knn2_station']
 del kdd_data['knn3_station']
 del kdd_data['knn4_station']
 
-#PM10二阶交叉项
+# Create new feature of cross relation between PM10 and other feature
+# PM10 二阶交叉项
 cox_PM10 = {'SO2' : 't1_SO2'
             ,'RH' : 't1_humidity'
             ,'P'  : 't1_pressure'
@@ -87,19 +96,21 @@ for key in cox_PM10:
     kdd_data['{var}*PM10'.format(var=cox_PM10[key])] = kdd_data[cox_PM10[key]] * kdd_data['t1_PM10']
 
 
-#其他交叉项
+# Create new feature of cross relation between other feature
+# 其他交叉项
 kdd_data['t1_NO2*RH'] = kdd_data['t1_NO2'] * kdd_data['t1_humidity']
 kdd_data['t1_O3*T'] = kdd_data['t1_O3'] * kdd_data['t1_temperature']
 
 
 
-#平移函数
+# shift function for time series feature
+# 平移函数
 def shift_data(df,grp_cols,shift_cols,shift_num,new_cols):
         df[new_cols] = df.groupby(grp_cols)[shift_cols].shift(shift_num)
         return( df )
 
-
-#自身站点数据;（温度、压强、湿度、风速、PM2.5、PM10、O3、NO2、CO、SO2）*（t1到t9）
+# list of basic features in t1 
+# 自身站点数据;（温度、压强、湿度、风速、PM2.5、PM10、O3、NO2、CO、SO2）*（t1到t9）
 dirt_zs = {'temperature' : 't1_temperature'
         ,'pressure' : 't1_pressure'
         ,'humidity' : 't1_humidity'
@@ -111,16 +122,19 @@ dirt_zs = {'temperature' : 't1_temperature'
         ,'CO'  : 't1_CO'
         ,'SO2' : 't1_SO2'
         }
+# shift the feature for 9 hours to create next 8 hour feature 
 for i in range(0,9):
     for  key in dirt_zs:
         shift_data(kdd_data,'station_id',dirt_zs[key],-i,'{var}_t{num}'.format(num=i+1,var=key))
 
-#风向 *（t1到t9）
+# shift the winddirection for 9 hours to create next 8 hour feature of winddirection
+# 风向 *（t1到t9）
 for i in range(0,9):
     shift_data(kdd_data,'station_id','t1_winddirection',-i,'winddirection_t{num}'.format(num=i+1))
 del kdd_data['t1_winddirection'] 
 
-#4个临近点气象站数据;（对应的温度、压强、湿度、风速、风向、PM2.5、PM10、O3、NO2、CO、SO2）*（t7到t9)
+# List of the basic feature about nearest four weather stations 
+# 4个临近点气象站数据;（对应的温度、压强、湿度、风速、风向、PM2.5、PM10、O3、NO2、CO、SO2）*（t7到t9)
 dirt_knn = {'knn1_temperature'   : 'knn1_temperature'
             ,'knn1_pressure'     : 'knn1_pressure'
             ,'knn1_humidity'     : 'knn1_humidity'
@@ -171,14 +185,17 @@ dirt_knn = {'knn1_temperature'   : 'knn1_temperature'
  
             }
 
+# shift the basic features of nearest four weather stations and obtain the last 3 hours of next 8 hours
 for i in range(6,9):
     for  key in dirt_knn:
         shift_data(kdd_data,'station_id',dirt_knn[key],-i,'{var}_t{num}'.format(num=i+1,var=key))
      
- 
-#取当日周几 0-6
+# Create the feature of weekday
+# 取当日周几 0-6
 kdd_data['week_day'] = pd.to_datetime(kdd_data.utc_time).dt.weekday.astype('uint8')
-#取月份month
+
+# Create the feature of month
+# 取月份month
 kdd_data['month'] = pd.to_datetime(kdd_data.utc_time).dt.month.astype('uint8') 
 
 cox_PM10 = {'SO2' : 't1_SO2'
@@ -188,7 +205,8 @@ cox_PM10 = {'SO2' : 't1_SO2'
             ,'CO' : 't1_CO'
             }
 
-#t8-t9 * 各种交互特征
+# Create cross related feature
+# t8-t9 * 各种交互特征
 cox_item = {'NO2*RH'    : 't1_NO2*RH'
             ,'O3*T'     : 't1_O3*T'
             ,'SO2*PM10' : 't1_SO2*PM10'
@@ -212,16 +230,19 @@ for i in range(7,9):
 #kdd_data['circle'][(kdd_data['station_id']=='huairou_aq') | (kdd_data['station_id']=='miyun_aq')| (kdd_data['station_id']=='dingling_aq')|(kdd_data['station_id']=='badaling_aq')|(kdd_data['station_id']=='yongledian_aq')|(kdd_data['station_id']=='yufa_aq')|(kdd_data['station_id']=='liulihe_aq')]=6
 #kdd_data['circle'][(kdd_data['station_id']=='pinggu_aq') | (kdd_data['station_id']=='yanqin_aq')| (kdd_data['station_id']=='miyunshuiku_aq')| (kdd_data['station_id']=='donggaocun_aq')]=7
 
-#小时 hour
+# Creat feature of hour
+# 小时 hour
 kdd_data['hour'] = pd.to_datetime(kdd_data.utc_time).dt.hour.astype('uint8')
-        
-#自身天气 t1_weather *（t9-t10）
+ 
+# Create weather feature of the 9th and 10th hour
+# 自身天气 t1_weather *（t9-t10）
 shift_data(kdd_data,'station_id','t1_weather',-8,'weather_t9')
 shift_data(kdd_data,'station_id','t1_weather',-9,'weather_t10')
 del kdd_data['t1_weather'] 
-   
- #(t10-t69)*(PM2.5 PM10 O3)
-#自身站点数据;（PM2.5、PM10、O3）*(t10-t70)
+
+# Shift the historical data to get next 60 hours prediction of PM2.5, PM10, O3
+# (t10-t69)*(PM2.5 PM10 O3)
+# 自身站点数据;（PM2.5、PM10、O3）*(t10-t70)
 dirt_zs2 = {
         'PM2.5':'t1_PM2.5'
         ,'PM10':'t1_PM10'
@@ -232,10 +253,12 @@ for i in range(9,69):
     for  key in dirt_zs2:
         shift_data(kdd_data,'station_id',dirt_zs2[key],-i,'{var}_t{num}'.format(num=i+1,var=key))
 
-##只保留hour为12 13 14的记录
-#kdd_data=kdd_data[(kdd_data.hour == 12)|(kdd_data.hour == 13)|(kdd_data.hour == 14)]
+# only reserve the record when hour in (12, 13, 14)
+# 只保留hour为12 13 14的记录
+# kdd_data=kdd_data[(kdd_data.hour == 12)|(kdd_data.hour == 13)|(kdd_data.hour == 14)]
 
-#删除无用变量
+# delete unuseful feature
+# 删除无用变量
 for key in dirt_zs:
     del kdd_data[dirt_zs[key]] 
 for key in dirt_knn:
